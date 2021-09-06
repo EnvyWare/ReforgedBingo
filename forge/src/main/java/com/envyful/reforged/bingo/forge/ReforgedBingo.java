8318009b0f85cee5e830c3900316f438c82bb382
danorris709 <dnorris443@gmail.com>
2021-09-06 09:59:54 +0100

    feat(forge): add main mod class

---
+++ forge/src/main/java/com/envyful/reforged/bingo/forge/ReforgedBingo.java
@@ -0,0 +1,36 @@
+package com.envyful.reforged.bingo.forge;
+
+import com.envyful.api.forge.command.ForgeCommandFactory;
+import com.envyful.api.forge.player.ForgePlayerManager;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(
+        modid = "reforgedbingo",
+        name = "Reforged Bingo Forge",
+        version = ReforgedBingo.VERSION,
+        acceptableRemoteVersions = "*"
+)
+public class ReforgedBingo {
+
+    protected static final String VERSION = "0.3.0";
+
+    private static ReforgedBingo instance;
+
+    private ForgePlayerManager playerManager = new ForgePlayerManager();
+    private ForgeCommandFactory commandFactory = new ForgeCommandFactory();
+
+    @Mod.EventHandler
+    public void onInit(FMLInitializationEvent event) {
+        instance = this;
+
+    }
+
+    public static ReforgedBingo getInstance() {
+        return instance;
+    }
+
+    public ForgePlayerManager getPlayerManager() {
+        return this.playerManager;
+    }
+}

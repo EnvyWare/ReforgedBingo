76d4bf498b1c06bdb794dc7de8b97de9106cb693
Daniel Norris <dnorris443@gmail.com>
2022-06-20 18:13:12 +0100

    feat(forge16): add GUI

---
+++ forge16/src/main/java/com/envyful/reforged/bingo/forge/ui/BingoCardUI.java
@@ -0,0 +1,47 @@
+package com.envyful.reforged.bingo.forge.ui;
+
+import com.envyful.api.config.type.ConfigItem;
+import com.envyful.api.forge.chat.UtilChatColour;
+import com.envyful.api.forge.config.UtilConfigItem;
+import com.envyful.api.gui.factory.GuiFactory;
+import com.envyful.api.gui.pane.Pane;
+import com.envyful.api.player.EnvyPlayer;
+import com.envyful.reforged.bingo.forge.ReforgedBingo;
+import com.envyful.reforged.bingo.forge.config.BingoConfig;
+import com.envyful.reforged.bingo.forge.player.BingoAttribute;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.ItemStack;
+
+public class BingoCardUI {
+
+    public static void open(EnvyPlayer<ServerPlayerEntity> player) {
+        BingoConfig config = ReforgedBingo.getInstance().getConfig();
+
+        Pane pane = GuiFactory.paneBuilder()
+                .topLeftY(0)
+                .topLeftX(0)
+                .height(config.getConfigInterface().getHeight())
+                .width(9)
+                .build();
+
+        BingoAttribute attribute = player.getAttribute(ReforgedBingo.class);
+
+        for (ConfigItem fillerItem : config.getConfigInterface().getFillerItems()) {
+            pane.add(GuiFactory.displayableBuilder(ItemStack.class)
+                             .itemStack(UtilConfigItem.fromConfigItem(fillerItem))
+                             .build());
+        }
+
+        UtilConfigItem.addConfigItem(pane, config.getHelpItem());
+
+        attribute.display(pane);
+
+        GuiFactory.guiBuilder()
+                .addPane(pane)
+                .height(6)
+                .title(UtilChatColour.translateColourCodes('&', config.getConfigInterface().getTitle()))
+                .setPlayerManager(ReforgedBingo.getInstance().getPlayerManager())
+                .setCloseConsumer(envyPlayer -> {})
+                .build().open(player);
+    }
+}

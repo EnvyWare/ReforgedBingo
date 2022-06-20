20216a63f6bf3883638f4585e899ecf3d8185317
Daniel Norris <dnorris443@gmail.com>
2022-06-20 18:12:52 +0100

    feat(forge16): add main mod instance

---
+++ forge16/src/main/java/com/envyful/reforged/bingo/forge/ReforgedBingo.java
@@ -0,0 +1,126 @@
+package com.envyful.reforged.bingo.forge;
+
+import com.envyful.api.concurrency.UtilConcurrency;
+import com.envyful.api.config.yaml.YamlConfigFactory;
+import com.envyful.api.database.Database;
+import com.envyful.api.database.impl.SimpleHikariDatabase;
+import com.envyful.api.forge.command.ForgeCommandFactory;
+import com.envyful.api.forge.concurrency.ForgeTaskBuilder;
+import com.envyful.api.forge.gui.factory.ForgeGuiFactory;
+import com.envyful.api.forge.player.ForgePlayerManager;
+import com.envyful.api.gui.factory.GuiFactory;
+import com.envyful.reforged.bingo.forge.command.BingoCardCommand;
+import com.envyful.reforged.bingo.forge.config.BingoConfig;
+import com.envyful.reforged.bingo.forge.config.BingoLocaleConfig;
+import com.envyful.reforged.bingo.forge.config.BingoQueries;
+import com.envyful.reforged.bingo.forge.listener.BingoCardCompleteListener;
+import com.envyful.reforged.bingo.forge.listener.PokemonCatchListener;
+import com.envyful.reforged.bingo.forge.player.BingoAttribute;
+import com.envyful.reforged.bingo.forge.task.CardResetTask;
+import com.pixelmonmod.pixelmon.api.pokemon.species.Species;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Objects;
+
+@Mod("reforgedbingo")
+public class ReforgedBingo {
+
+    private static ReforgedBingo instance;
+
+    private ForgePlayerManager playerManager = new ForgePlayerManager();
+    private ForgeCommandFactory commandFactory = new ForgeCommandFactory();
+
+    private BingoConfig config;
+    private BingoLocaleConfig locale;
+    private Database database;
+
+    public ReforgedBingo() {
+        instance = this;
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onInit(FMLServerAboutToStartEvent event) {
+        GuiFactory.setPlatformFactory(new ForgeGuiFactory());
+
+        this.reloadConfig();
+        this.playerManager.registerAttribute(this, BingoAttribute.class);
+
+        new BingoCardCompleteListener(this);
+        new PokemonCatchListener(this);
+
+        UtilConcurrency.runAsync(() -> {
+            this.database = new SimpleHikariDatabase(this.config.getDatabase());
+
+            try (Connection connection = this.database.getConnection();
+                 PreparedStatement preparedStatement = connection.prepareStatement(BingoQueries.CREATE_TABLE)) {
+                preparedStatement.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
+
+        new ForgeTaskBuilder()
+                .async(true)
+                .delay(10L)
+                .interval(10L)
+                .task(new CardResetTask(this))
+                .start();
+    }
+
+    public void reloadConfig() {
+        try {
+            this.config = YamlConfigFactory.getInstance(BingoConfig.class);
+            this.locale = YamlConfigFactory.getInstance(BingoLocaleConfig.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SubscribeEvent
+    public void onServerStart(RegisterCommandsEvent event) {
+        this.commandFactory.registerCommand(event.getDispatcher(), new BingoCardCommand());
+    }
+
+    public static ReforgedBingo getInstance() {
+        return instance;
+    }
+
+    public ForgePlayerManager getPlayerManager() {
+        return this.playerManager;
+    }
+
+    public BingoConfig getConfig() {
+        return this.config;
+    }
+
+    public BingoLocaleConfig getLocale() {
+        return this.locale;
+    }
+
+    public Database getDatabase() {
+        return this.database;
+    }
+
+    public boolean isBlacklisted(Species pokemon) {
+        if (pokemon.isLegendary() || pokemon.isUltraBeast()) {
+            return true;
+        }
+
+        for (Species blacklistedSpawn : this.getConfig().getBlacklistedSpawns()) {
+            if (Objects.equals(blacklistedSpawn, pokemon)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}

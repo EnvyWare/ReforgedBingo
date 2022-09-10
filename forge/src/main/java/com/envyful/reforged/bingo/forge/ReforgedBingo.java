0f9b341ff40a7d27ebe7d99376e54475169bcb3d
Daniel Norris <dnorris443@gmail.com>
2022-09-10 18:19:04 +0100

    chore(gradle): update API & version

--- forge/src/main/java/com/envyful/reforged/bingo/forge/ReforgedBingo.java
+++ forge/src/main/java/com/envyful/reforged/bingo/forge/ReforgedBingo.java
@@ -37,7 +37,7 @@ import java.util.Objects;
 )
 public class ReforgedBingo {
 
-    protected static final String VERSION = "2.3.0";
+    protected static final String VERSION = "2.3.1";
 
     private static ReforgedBingo instance;
 

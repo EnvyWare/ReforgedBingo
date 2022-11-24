fddb3ad16690d1d26d0e78eaaf90a925cb0a64b1
Daniel Norris <dnorris443@gmail.com>
2022-11-24 23:42:14 +0000

    chore(gradle): bump version to 2.4.5

--- forge/src/main/java/com/envyful/reforged/bingo/forge/ReforgedBingo.java
+++ forge/src/main/java/com/envyful/reforged/bingo/forge/ReforgedBingo.java
@@ -37,7 +37,7 @@ import java.util.Objects;
 )
 public class ReforgedBingo {
 
-    protected static final String VERSION = "2.4.4";
+    protected static final String VERSION = "2.4.5";
 
     private static ReforgedBingo instance;
 

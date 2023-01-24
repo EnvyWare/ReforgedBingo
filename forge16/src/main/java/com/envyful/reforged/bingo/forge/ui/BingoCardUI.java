b80eae8aac7f7268648554af20077abe5f7923b0
Daniel Norris <dnorris443@gmail.com>
2023-01-24 17:05:16 +0000

    fix(ui): make lore for book display properly

--- forge16/src/main/java/com/envyful/reforged/bingo/forge/ui/BingoCardUI.java
+++ forge16/src/main/java/com/envyful/reforged/bingo/forge/ui/BingoCardUI.java
@@ -7,10 +7,13 @@ import com.envyful.api.forge.player.ForgeEnvyPlayer;
 import com.envyful.api.gui.factory.GuiFactory;
 import com.envyful.api.gui.pane.Pane;
 import com.envyful.api.player.EnvyPlayer;
+import com.envyful.api.text.parse.SimplePlaceholder;
 import com.envyful.reforged.bingo.forge.ReforgedBingo;
 import com.envyful.reforged.bingo.forge.config.BingoConfig;
 import com.envyful.reforged.bingo.forge.player.BingoAttribute;
 import net.minecraft.entity.player.ServerPlayerEntity;
+
+import java.util.stream.Collectors;
 
 public class BingoCardUI {
 
@@ -28,8 +31,7 @@ public class BingoCardUI {
 
         UtilConfigInterface.fillBackground(pane, config.getConfigInterface());
 
-        UtilConfigItem.builder().extendedConfigItem((ForgeEnvyPlayer) player, pane, config.getHelpItem());
-
+        UtilConfigItem.builder().extendedConfigItem((ForgeEnvyPlayer) player, pane, config.getHelpItem(), (SimplePlaceholder) s -> s);
         attribute.display(pane);
 
         GuiFactory.guiBuilder()

b2a79250e4449bc0dfe7b0ffc4d4e3c0651f9a91
Daniel Norris <dnorris443@gmail.com>
2023-12-02 15:20:45 +0000

    fix(forge16): move default positions one over to the left

--- forge16/src/main/java/com/envyful/reforged/bingo/forge/config/BingoConfig.java
+++ forge16/src/main/java/com/envyful/reforged/bingo/forge/config/BingoConfig.java
@@ -92,10 +92,10 @@ public class BingoConfig extends AbstractYamlConfig {
     ), 4, 0, Maps.newHashMap());
 
     private List<Integer> cardPositions = Lists.newArrayList(
-            11, 12, 13, 14, 15, 16, 17,
-            20, 21, 22, 23, 24, 25, 26,
-            29, 30, 31, 32, 33, 34, 35,
-            38, 39, 40, 41, 42, 43, 44
+            10, 11, 12, 13, 14, 15, 16,
+            19, 20, 21, 22, 23, 24, 25,
+            28, 29, 30, 31, 32, 33, 34,
+            37, 38, 39, 40, 41, 42, 43
     );
 
     public BingoConfig() {

e122f76774a7eee4898316a5a021d5b02229a207
Daniel Norris <dnorris443@gmail.com>
2024-11-30 22:42:39 +0000

    feat(forge16): add attribute adapters

--- forge16/src/main/java/com/envyful/reforged/bingo/forge/player/BingoAttribute.java
+++ forge16/src/main/java/com/envyful/reforged/bingo/forge/player/BingoAttribute.java
@@ -1,20 +1,18 @@
 package com.envyful.reforged.bingo.forge.player;
 
-import com.envyful.api.forge.chat.UtilChatColour;
 import com.envyful.api.forge.config.UtilConfigItem;
 import com.envyful.api.forge.items.ItemBuilder;
 import com.envyful.api.forge.player.ForgeEnvyPlayer;
 import com.envyful.api.forge.player.attribute.ManagedForgeAttribute;
 import com.envyful.api.gui.factory.GuiFactory;
 import com.envyful.api.gui.pane.Pane;
-import com.envyful.api.json.UtilGson;
+import com.envyful.api.platform.PlatformProxy;
 import com.envyful.api.player.save.attribute.DataDirectory;
 import com.envyful.api.reforged.pixelmon.UtilPokemonInfo;
 import com.envyful.api.reforged.pixelmon.sprite.UtilSprite;
 import com.envyful.api.text.Placeholder;
 import com.envyful.reforged.bingo.forge.ReforgedBingo;
 import com.envyful.reforged.bingo.forge.config.BingoConfig;
-import com.envyful.reforged.bingo.forge.config.BingoQueries;
 import com.envyful.reforged.bingo.forge.event.BingoSlotCompleteEvent;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
@@ -26,24 +24,21 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.common.MinecraftForge;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @DataDirectory("config/players/ReforgedBingo/")
 public class BingoAttribute extends ManagedForgeAttribute<ReforgedBingo> {
 
-    private long started;
-    private CardSlot[][] bingoCard;
-    private int completed = 0;
+    protected long started;
+    protected CardSlot[][] bingoCard;
+    protected int completed = 0;
 
-    public BingoAttribute() {
-        super(ReforgedBingo.getInstance());
+    public BingoAttribute(UUID id) {
+        super(id, ReforgedBingo.getInstance());
     }
 
     public int getCompleted() {
@@ -66,73 +61,37 @@ public class BingoAttribute extends ManagedForgeAttribute<ReforgedBingo> {
         this.checkCardSize();
     }
 
-    @Override
-    public void load() {
-        try (Connection connection = this.manager.getDatabase().getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(BingoQueries.LOAD_PLAYER_BINGO_CARD)) {
-            preparedStatement.setString(1, this.id.toString());
-
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            if (!resultSet.next()) {
-                return;
-            }
-
-            this.started = resultSet.getLong("timeStarted");
-            this.bingoCard = UtilGson.GSON.fromJson(resultSet.getString("card"), CardSlot[][].class);
-            this.completed = resultSet.getInt("completedCards");
-        } catch (SQLException e) {
-            ReforgedBingo.getLogger().catching(e);
-        }
-    }
-
-    @Override
-    public void save() {
-        try (Connection connection = this.manager.getDatabase().getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(BingoQueries.UPDATE_PLAYER_BINGO_CARD)) {
-
-            preparedStatement.setString(1, this.id.toString());
-            preparedStatement.setString(2, UtilGson.GSON.toJson(this.bingoCard));
-            preparedStatement.setLong(3, this.started);
-            preparedStatement.setInt(4, this.completed);
-
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            ReforgedBingo.getLogger().catching(e);
-        }
-    }
-
     private void checkCardSize() {
-        if (this.bingoCard.length != this.manager.getConfig().getHeight()) {
+        if (this.bingoCard.length != ReforgedBingo.getConfig().getHeight()) {
             this.generateNewCard();
-            ReforgedBingo.getLogger().error("Card size mismatch for player {}. Regenerating card...", this.id.toString());
+            ReforgedBingo.getLogger().error("Card size mismatch for player {}. Regenerating card...", this.id);
         }
 
         for (CardSlot[] cardSlots : this.bingoCard) {
-            if (cardSlots.length != this.manager.getConfig().getWidth()) {
+            if (cardSlots.length != ReforgedBingo.getConfig().getWidth()) {
                 this.generateNewCard();
-                ReforgedBingo.getLogger().error("Card size mismatch for player {}. Regenerating card...", this.id.toString());
+                ReforgedBingo.getLogger().error("Card size mismatch for player {}. Regenerating card...", this.id);
                 return;
             }
         }
     }
 
     public boolean checkCardExpiry() {
-        if (ReforgedBingo.getInstance().getConfig().isStaticResetTimeEnabled()) {
+        if (ReforgedBingo.getConfig().isStaticResetTimeEnabled()) {
             return System.currentTimeMillis() > this.started;
         }
 
-        return (System.currentTimeMillis() - started) > TimeUnit.SECONDS.toMillis(ReforgedBingo.getInstance().getConfig().getCardDurationSeconds());
+        return (System.currentTimeMillis() - started) > TimeUnit.SECONDS.toMillis(ReforgedBingo.getConfig().getCardDurationSeconds());
     }
 
     public void generateNewCard() {
-        this.bingoCard = new CardSlot[this.manager.getConfig().getHeight()][this.manager.getConfig().getWidth()];
+        this.bingoCard = new CardSlot[ReforgedBingo.getConfig().getHeight()][ReforgedBingo.getConfig().getWidth()];
         Map<String, Integer> counters = Maps.newHashMap();
 
-        for (int y = 0; y < this.manager.getConfig().getHeight(); y++) {
+        for (int y = 0; y < ReforgedBingo.getConfig().getHeight(); y++) {
             CardSlot[] currentLine = this.bingoCard[y];
 
-            for (int x = 0; x < this.manager.getConfig().getWidth(); x++) {
+            for (int x = 0; x < ReforgedBingo.getConfig().getWidth(); x++) {
                 Species species = PixelmonSpecies.getRandomSpecies();
 
                 while (!this.canPickPokemon(species, counters)) {
@@ -145,15 +104,15 @@ public class BingoAttribute extends ManagedForgeAttribute<ReforgedBingo> {
 
         this.started = System.currentTimeMillis();
 
-        if (this.manager.getConfig().isStaticResetTimeEnabled()) {
-            this.started = this.manager.getConfig().getStaticResetTime().nextExecution(ZonedDateTime.now()).get().toInstant().toEpochMilli();
+        if (ReforgedBingo.getConfig().isStaticResetTimeEnabled()) {
+            this.started = ReforgedBingo.getConfig().getStaticResetTime().nextExecution(ZonedDateTime.now()).get().toInstant().toEpochMilli();
         }
 
-        this.parent.message(UtilChatColour.colour(this.manager.getLocale().getCardReset()));
+        this.parent.message(ReforgedBingo.getLocale().getCardReset());
     }
 
     private boolean canPickPokemon(Species value, Map<String, Integer> counters) {
-        if (value.getDefaultForm().getPreEvolutions().size() >= this.manager.getConfig().getMaximumEvolution()) {
+        if (value.getDefaultForm().getPreEvolutions().size() >= ReforgedBingo.getConfig().getMaximumEvolution()) {
             return false;
         }
 
@@ -163,7 +122,7 @@ public class BingoAttribute extends ManagedForgeAttribute<ReforgedBingo> {
 
         Pokemon pokemon = PokemonFactory.create(value);
 
-        for (BingoConfig.BoardFilter filter : ReforgedBingo.getInstance().getConfig().getFilters()) {
+        for (BingoConfig.BoardFilter filter : ReforgedBingo.getConfig().getFilters()) {
             if (!filter.getSpec().matches(pokemon)) {
                 continue;
             }
@@ -186,7 +145,7 @@ public class BingoAttribute extends ManagedForgeAttribute<ReforgedBingo> {
         for (CardSlot[] cardSlots : this.bingoCard) {
             for (int i = 0; i < cardSlots.length; i++) {
                 CardSlot cardSlot = cardSlots[i];
-                if (cardSlot.getSpecies() == species && !cardSlot.isComplete()) {
+                if (cardSlot.getSpecies().getDex() == species.getDex() && !cardSlot.isComplete()) {
                     cardSlot.setComplete(true);
 
                     boolean rowComplete = this.checkRowCompletion(cardSlots);
@@ -257,11 +216,11 @@ public class BingoAttribute extends ManagedForgeAttribute<ReforgedBingo> {
     }
 
     public long getTimeRemaining() {
-        if (this.manager.getConfig().isStaticResetTimeEnabled()) {
+        if (ReforgedBingo.getConfig().isStaticResetTimeEnabled()) {
             return (TimeUnit.MILLISECONDS.toHours(this.started - System.currentTimeMillis()));
         }
 
-        return (TimeUnit.SECONDS.toHours(ReforgedBingo.getInstance().getConfig().getCardDurationSeconds()) - TimeUnit.MILLISECONDS.toHours(System.currentTimeMillis() - this.started));
+        return (TimeUnit.SECONDS.toHours(ReforgedBingo.getConfig().getCardDurationSeconds()) - TimeUnit.MILLISECONDS.toHours(System.currentTimeMillis() - this.started));
     }
 
     public void display(Pane pane) {
@@ -269,13 +228,13 @@ public class BingoAttribute extends ManagedForgeAttribute<ReforgedBingo> {
 
         int slot = 0;
 
-        for (int y = 0; y < this.manager.getConfig().getHeight(); y++) {
-            for (int x = 0; x < this.manager.getConfig().getWidth(); x++) {
-                int position = this.manager.getConfig().getCardPositions().get(slot);
+        for (int y = 0; y < ReforgedBingo.getConfig().getHeight(); y++) {
+            for (int x = 0; x < ReforgedBingo.getConfig().getWidth(); x++) {
+                int position = ReforgedBingo.getConfig().getCardPositions().get(slot);
                 ++slot;
 
                 if (this.bingoCard[y][x].isComplete()) {
-                    pane.set(position % 9, position / 9, GuiFactory.displayable(UtilConfigItem.fromConfigItem(ReforgedBingo.getInstance().getConfig().getCompleteItem(),
+                    pane.set(position % 9, position / 9, GuiFactory.displayable(UtilConfigItem.fromConfigItem(ReforgedBingo.getConfig().getCompleteItem(),
                             Placeholder.simple("%pokemon%", this.bingoCard[y][x].getSpecies().getLocalizedName()))));
                     continue;
                 }
@@ -284,13 +243,13 @@ public class BingoAttribute extends ManagedForgeAttribute<ReforgedBingo> {
 
                 List<ITextComponent> lore = Lists.newArrayList();
 
-                for (String s : ReforgedBingo.getInstance().getLocale().getCardSlotLore()) {
-                    lore.add(UtilChatColour.colour(s
-                            .replace("%biomes%", String.join(ReforgedBingo.getInstance().getLocale().getBiomeInfoDelimiter(), UtilPokemonInfo.getSpawnBiomes(cardSlot.getSpecies().getDefaultForm())))
-                            .replace("%catch_rate%", String.join(ReforgedBingo.getInstance().getLocale().getCatchRateDelimiter(), UtilPokemonInfo.getCatchRate(cardSlot.getSpecies().getDefaultForm())))
+                for (String s : ReforgedBingo.getLocale().getCardSlotLore()) {
+                    lore.add(PlatformProxy.parse(s
+                            .replace("%biomes%", String.join(ReforgedBingo.getLocale().getBiomeInfoDelimiter(), UtilPokemonInfo.getSpawnBiomes(cardSlot.getSpecies().getDefaultForm())))
+                            .replace("%catch_rate%", String.join(ReforgedBingo.getLocale().getCatchRateDelimiter(), UtilPokemonInfo.getCatchRate(cardSlot.getSpecies().getDefaultForm())))
                             .replace("%spawn_times%", String.join(
-                            ReforgedBingo.getInstance().getLocale().getSpawnTimesDelimiter(),
-                            UtilPokemonInfo.getSpawnTimes(cardSlot.getSpecies().getDefaultForm())))));
+                                    ReforgedBingo.getLocale().getSpawnTimesDelimiter(),
+                                    UtilPokemonInfo.getSpawnTimes(cardSlot.getSpecies().getDefaultForm())))));
                 }
 
                 pane.set(position % 9, position / 9,
@@ -301,7 +260,7 @@ public class BingoAttribute extends ManagedForgeAttribute<ReforgedBingo> {
                                         .build())
                                 .asyncClick(false)
                                 .clickHandler((envyPlayer, clickType) -> {
-                                    for (String cardSlotCommand : ReforgedBingo.getInstance().getConfig().getCardSlotCommands()) {
+                                    for (String cardSlotCommand : ReforgedBingo.getConfig().getCardSlotCommands()) {
                                         envyPlayer.executeCommand(cardSlotCommand.replace(
                                                 "%pokemon%",
                                                 cardSlot.getSpecies().getLocalizedName()
